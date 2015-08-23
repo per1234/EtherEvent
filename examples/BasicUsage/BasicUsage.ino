436c2bedbc4de9f57533cf82ac0426abb5575564
per1234 <per1234@users.noreply.github.com>
2015-08-22 22:49:56 -0700

    Example sketch DHCP code improvements
    - Change #define to const
    - Call Ethernet.maintain() to renew DHCP lease

--- examples/BasicUsage/BasicUsage.ino
+++ examples/BasicUsage/BasicUsage.ino
@@ -10,11 +10,9 @@
 
 
 //configuration parameters - modify these values to your desired settings
-#define DHCP false  //true==use DHCP to assign an IP address to the device, this will significantly increase memory usage. false==use static IP address.
+const boolean useDHCP = false;  //true==use DHCP to assign an IP address to the device, this will significantly increase memory usage. false==use static IP address.
 byte MACaddress[] = {0, 1, 2, 3, 4, 4};  //this can be anything you like, but must be unique on your network
-#if DHCP == false
 const IPAddress deviceIP = IPAddress(192, 168, 69, 104);  //IP address to use for the device. This can be any valid address on the network as long as it is unique. If you are using DHCP then this doesn't need to be configured.
-#endif
 const char password[] = "password";  //EtherEvent password. This must match the password set in EventGhost.
 const unsigned int port = 1024;  //TCP port to receive events
 
@@ -30,11 +28,12 @@ unsigned long sendTimeStamp;  //used by the example to periodically send an even
 
 void setup() {
   Serial.begin(9600);  //the received event and other information will be displayed in your serial monitor while the sketch is running
-#if DHCP == true
-  Ethernet.begin(MACaddress);  //let the network assign an IP address
-#else
-  Ethernet.begin(MACaddress, deviceIP);  //use static IP address
-#endif
+  if (useDHCP == true) {
+    Ethernet.begin(MACaddress);  //let the network assign an IP address
+  }
+  else {
+    Ethernet.begin(MACaddress, deviceIP);  //use static IP address
+  }
   ethernetServer.begin();  //begin the server that will be used to receive events
   if (EtherEvent.begin() == false || EtherEvent.setPassword(password) == false) {  //initialize EtherEvent and set your password
     Serial.println(F("ERROR: Buffer size exceeds available memory, use smaller values."));
@@ -69,6 +68,10 @@ void loop() {
     else {
       Serial.println(F("Event send failed"));
     }
+  }
+
+  if (useDHCP == true) {
+    Ethernet.maintain();  //request renewal of DHCP lease if expired
   }
 }
 
