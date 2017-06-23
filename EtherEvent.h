ba4c70ebca42788f2b15c956171e562697cdfbf3
per1234 <accounts@perglass.com>
2017-06-22 20:20:24 -0700

    Change variable name to avoid double negatives in payload test

--- EtherEvent.h
+++ EtherEvent.h
@@ -294,7 +294,7 @@ class EtherEventClass {
     boolean send(EthernetClient &ethernetClient, const target_t target, const unsigned int port, const event_t event) {
       ETHEREVENT_SERIAL.println(F("EtherEvent.send(no payload argument)"));
 #ifndef ETHEREVENT_FAST_SEND
-      noPayload = true;  //In non-ETHEREVENT_FAST_SEND mode, send() has no way to know the type of payload so the best way to detect whether a payload was specified is to set a flag based on the function signature, which allows send() to be more efficient due to not needing to handle the non-existent payload.
+      payloadSpecified = false;  //In non-ETHEREVENT_FAST_SEND mode, send() has no way to know the type of payload so the best way to detect whether a payload was specified is to set a flag based on the function signature, which allows send() to be more efficient due to not needing to handle the non-existent payload.
 #endif  //ETHEREVENT_FAST_SEND
       return send(ethernetClient, target, port, event, "", DEFAULT_PASSWORD_STRING);
     }
@@ -655,7 +655,7 @@ class EtherEventClass {
       }
 #endif  //ETHEREVENT_NO_AUTHENTICATION
 #ifndef ETHEREVENT_FAST_SEND
-      if (noPayload == false)  //check if there is a payload
+      if (payloadSpecified == true)  //check if there is a payload
 #else  //ETHEREVENT_FAST_SEND
       if (payload[0] != 0)  //check if there is a payload
 #endif  //ETHEREVENT_FAST_SEND
@@ -749,7 +749,7 @@ class EtherEventClass {
     char* password;
     byte passwordLength;
 
-    boolean noPayload;
+    boolean payloadSpecified;
 
     IPAddress fromIP;  //IP address of the last event sender
     byte eventLengthMax;
