dc94a518ff236cb5d89716bf7ff44169828fe5ca
per1234 <accounts@perglass.com>
2018-03-24 20:46:13 -0700

    Add a HIL test library
    Previously the test sketches were primarily used for compilation testing without having effective mechanisms to check whether the library actually works. This change adds "hardware in loop" (HIL) test functionality that will allow the tests to be run in conjunction with special EventGhost trees in order to verify that the sent data matches the received data. The code is put in a library to enable it to be shared between a sketch for each combination of EtherEvent library preprocessor configurations.

--- extras/tests/FastSend1/FastSend1.ino
+++ extras/tests/FastSend1/FastSend1.ino
@@ -1,4 +1,29 @@
 // Used for testing EtherEvent.send() overloading in ETHEREVENT_FAST_SEND mode
+// Use with extras/tests/EventGhost-trees/EchoTCPEventsAuthenticated.egtree or EchoNESR.egtree
 
-//#define ETHEREVENT_NO_AUTHENTICATION  //this line must come before #include <EtherEvent.h>
+//#define ETHEREVENT_NO_AUTHENTICATION  //this line must come before #include <EtherEvent.h> //https://github.com/per1234/EtherEvent
 #define ETHEREVENT_FAST_SEND
+#define TEST_BATCH 1
+
+#include <SPI.h>  //bundled with Arduino hardware packages
+#include <Ethernet.h> //built-in library included with Arduino IDE
+#ifndef __ARDUINO_X86__
+#include <utility/w5100.h>  //Part of built-in Ethernet library included with Arduino IDE. Used for setting the W5x00 retransmission time and count.
+#endif //__ARDUINO_X86__
+
+#if !defined(ETHEREVENT_NO_AUTHENTICATION) && !defined(ESP8266)
+#include <MD5.h>  //http://github.com/tzikis/ArduinoMD5
+#endif  //!defined(ETHEREVENT_NO_AUTHENTICATION) && !defined(ESP8266)
+
+#include <EtherEvent.h> //https://github.com/per1234/EtherEvent
+
+#ifdef __AVR__
+#include <MemoryFree.h> //https://github.com/McNeight/MemoryFree
+#endif  //__AVR__
+#include <EtherEventTests.h>  //https://github.com/per1234/EtherEvent/tree/master/extras/tests/EtherEventTests
+
+void setup() {
+  etherEventTests();
+}
+
+void loop() {}
