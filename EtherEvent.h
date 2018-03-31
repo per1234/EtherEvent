529766fa51cbc9b1664d3609663d0e18a72e35aa
per1234 <accounts@perglass.com>
2018-03-31 02:50:58 -0700

    (f) move template parameter list inside preprocessor directive

--- EtherEvent.h
+++ EtherEvent.h
@@ -795,8 +795,8 @@ class EtherEventClass {
 
     //event conversions
 
-    template <typename event_t>
 #ifdef ETHEREVENT_NO_AUTHENTICATION
+    template <typename event_t>
     boolean send(EthernetClient &ethernetClient, const IPAddress &target, const unsigned int port, const event_t event) {
       ETHEREVENT_SERIAL.println(F("EtherEvent.send(no payload)"));
       return send(ethernetClient, target, port, event, "");
